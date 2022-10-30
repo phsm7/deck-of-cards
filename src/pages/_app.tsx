63b45b05d48d061b254e30c507fbb8166905a029
Pedro Machado <pedrohsmachado77@gmail.com>
2022-10-30 15:40:24 -0300

    create project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components'
+import Global from 'styles/global';
+import theme from 'styles/theme';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+       <Head>
+          <title>Deck of Cards</title>
+          <meta
+            name="description"
+            content="NextJS, React, TypeScript and Styled Components"
+          />
+        </Head>
+        <Global />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
