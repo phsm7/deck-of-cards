6848bf2dc051313c5b8a77e3e2746feaf374c780
Pedro Machado <pedrohsmachado77@gmail.com>
2022-10-30 16:23:15 -0300

    feat(theme): create theme and global styles config

---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+const theme = {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '1.4rem'
+  },
+  colors: {
+    primary: 'purple',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#F2F2F2',
+    error: 'red',
+  },
+  font: {
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
+    }
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+};
+
+export default theme;
