6208b5a35020e12e4e66ca50c5723c1968f4ac9d
Pedro Machado <pedrohsmachado77@gmail.com>
2022-11-09 23:20:14 -0300

    refactor: heading style

--- src/pages/mydeck/index.tsx
+++ src/pages/mydeck/index.tsx
@@ -109,21 +109,3 @@ export default function DeckPage() {
     </>
   )
 }
-
-/*
-
-  Construir uma tela que exibe cinco cartas, o conteúdo dessas cartas deve ser alimentado via API.
-
-  Cada carta deve conter pelo menos: nome, imagem, descrição e um valor aleatório de 0 a 10 que podemos chamar de pontos.
-
-  Ao acessar a primeira página ele deve digitar um nome e clicar em ver cartas.
-
-  Na tela seguinte ele visualiza as 5 cartas e o  seu nome no canto superior direito.
-
-  Na tela de cartas ele tem dois botões, onde um deles permite puxar uma nova carta aleatoriamente, ele pode apertar apenas 3 vezes.
-
-  As cartas nunca são descartadas, caso ele puxe 3 novas cartas ele estará visualizando 8 cartas.
-
-  O segundo botão permite a ele embaralhar a ordem das cartas que está visualizando.
-
-*/
