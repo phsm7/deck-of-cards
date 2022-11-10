51cf75da27ab4b22ac5910d6267474032182d7fd
Pedro Machado <pedrohsmachado77@gmail.com>
2022-11-09 22:09:09 -0300

    feat(_app): add context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import AppProvider from 'contexts/appProvider'
 import GlobalStyle from 'styles/global'
 import theme from 'styles/theme'
 
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <AppProvider>
         <GlobalStyle />
         <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      </AppProvider>
+    </ThemeProvider>
   )
 }
