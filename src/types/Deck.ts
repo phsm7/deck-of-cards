3fe005905c124db21dec3a4c538603f74bf10b81
Pedro Machado <pedrohsmachado77@gmail.com>
2022-11-09 22:03:46 -0300

    chore: change types

---
+++ src/types/Deck.ts
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+import React, { SetStateAction } from 'react';
+import { ErrorProps } from './Error';
+
+export type DeckProviderProps = {
+  children: React.ReactNode;
+};
+
+
+export type NewDeckProps = string;
+
+type ImagesProps = {
+  svg: string;
+  png: string;
+}
+
+export type CardProps = {
+  code: string;
+  image: string; 
+  images: ImagesProps;
+  value: string;
+  suit: string;
+}
+
+export type DeckResultProps = {
+    success: boolean;
+    deck_id: string;
+    cards: CardProps[];
+    remaining: number;
+};
+
+export type DeckShuffleProps = {
+  success: boolean;
+  deck_id: string;
+  shuffled: boolean;
+  remaining: number;
+}
+
+export type DrawCardProps = {
+  success: boolean;
+  deck_id: string; 
+  cards: CardProps;
+  remaining: number;
+}
+
+export type DeckTypes = {
+  loading: boolean; 
+  setLoading: React.Dispatch<SetStateAction<boolean>>;
+  deckId: NewDeckProps;
+  cards: CardProps[];
+  error: ErrorProps;
+  setError: React.Dispatch<SetStateAction<ErrorProps>>;
+  setCards: React.Dispatch<SetStateAction<CardProps[]>>;
+  setDeckId: React.Dispatch<SetStateAction<string>>;
+  getNewDeck:  () => Promise<DeckResultProps>;
+  shuffleDeck: (id: string) => Promise<DeckShuffleProps>;
+  drawNewCard: () => Promise<DrawCardProps>;
+}
--- styled.d.ts
+++ styled.d.ts
@@ -1,42 +1,8 @@
 import 'styled-components'
+import theme from 'styles/theme';
+theme
 
+type ApplicationTheme = typeof theme
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    grid: {
-        container: string;
-        gutter: string;
-    },
-    border: {
-        radius: string;
-    },
-    colors: {
-        primary: string;
-        white: string;
-        black: string;
-        gray: string;
-        error: string;
-    },
-    font: {
-        normal: number;
-        bold: number;
-        sizes: {
-            xsmall: string;
-            small: string;
-            medium: string;
-            large: string;
-            xlarge: string;
-            xxlarge: string;
-            huge: string;
-        }
-    },
-    spacings: {
-        xxsmall: string;
-        xsmall: string;
-        small: string;
-        medium: string;
-        large: string;
-        xlarge: string;
-        xxlarge: string;
-    },
-  }
+  export interface DefaultTheme extends ApplicationTheme {}
 }
